289ff390e800294088a9475c992b6c4e5cd1e430
Angie <angiecorteztay1@hotmail.com>
2019-04-05 12:33:33 -0500

    Updates

---
+++ precache-manifest.c279c51af28f6bf98009e64b9908513f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "494ac75f5d4408571d5e",
+    "url": "/crud-react-hooks/static/js/runtime~main.0bbbbcec.js"
+  },
+  {
+    "revision": "314d2a5b6b2ade2778d6",
+    "url": "/crud-react-hooks/static/js/main.c94fe321.chunk.js"
+  },
+  {
+    "revision": "bb57b066151d2241c76f",
+    "url": "/crud-react-hooks/static/js/2.942b1129.chunk.js"
+  },
+  {
+    "revision": "314d2a5b6b2ade2778d6",
+    "url": "/crud-react-hooks/static/css/main.66c81604.chunk.css"
+  },
+  {
+    "revision": "a4b7c708e2c8e3a33959bffaa53e5e33",
+    "url": "/crud-react-hooks/index.html"
+  }
+];
